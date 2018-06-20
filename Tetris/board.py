b1d5e8fbcbcda44639baa35d712bb967be4f16c8
Q-Mart <qumarthjash@gmail.com>
2018-06-20 09:50:44 +0100

    Change to 24 rows in board

--- Tetris/board.py
+++ Tetris/board.py
@@ -3,27 +3,23 @@ class Board():
 
     def __init__(self):
         self.__BLANK = 0
-        self.__data = [[self.__BLANK for x in range(10)] for y in range(20)]
-
-        # for x in range(10):
-        #     for y in range(20):
-        #         self.__data[y][x] = random.randrange(0, 6)
+        self.__data = [[self.__BLANK for x in range(10)] for y in range(24)]
 
         for x in range(10):
-            self.__data[19][x] = 1
+            self.__data[23][x] = 1
 
             if x%2 == 0:
-                self.__data[18][x] = 2
+                self.__data[22][x] = 2
 
     def __getColour(self):
         return random.randrange(0,6)
 
     def get(self):
-        return self.__data
+        return self.__data[4:]
 
     def cleanRows(self):
         # check if a row is full, deletes it and shifts above rows down
-        for y in range(19, -1, -1):
+        for y in range(23, 1, -1):
             full = True
             for x in range(10):
                 if self.__data[y][x] == self.__BLANK:
